469bef1d22d9842115352200f16fef15f12befa5
Adam Fitzpatrick <C00273365@setu.ie>
2024-10-22 09:27:17 +0100

    File preparation

--- Task1/FitzpatrickAdamTestTask1.java
+++ Task1/FitzpatrickAdamTestTask1.java
@@ -1,2 +1,11 @@
+package cm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FitzpatrickAdamTestTask1 {
+    @Test
+    public void test() {
+    }
 }
